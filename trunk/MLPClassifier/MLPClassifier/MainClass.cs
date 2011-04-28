22beb0e912474134b62a2e6b5b0cf684a58939c8
janca.andrei@gmail.com <janca.andrei@gmail.com@489428b5-2de9-d710-ef6a-e1c4df9b4134>
2011-04-28 16:45:21 +0000

    updated NN code - small fixes

--- trunk/MLPClassifier/MLPClassifier/MainClass.cs
+++ trunk/MLPClassifier/MLPClassifier/MainClass.cs
@@ -10,22 +10,30 @@ namespace MLPClassifier
     {
         static void Main(string[] args)
         {
-            /*MLP mlp = new MLP(40, 40, new int[] { 100, 10, 4 });
-            System.Console.WriteLine("init network");
-            mlp.learn(@"e:\eclipse workspace\FeatGen\trainingsetext\");
-            mlp.save("neuralnet.txt");*/
 
-            MLP mlp = new MLP("neuralnet.txt");
-            System.Console.WriteLine("NN loaded");
+            bool learn = true;
+            if (learn == true)
+            {
+                MLP mlp = new MLP(40, 40, new int[] { 100, 10, 4 });
+                System.Console.WriteLine("init network");
+                mlp.learn(@"e:\eclipse workspace\FeatGen\trainingsetext\");
+                mlp.save("neuralnet.txt");
+            }
+            else
+            {
+                MLP mlp = new MLP("neuralnet.txt");
+                System.Console.WriteLine("NN loaded");
 
-            Feature image = new Feature("1_0.bmp", false);
-            System.Console.WriteLine("picture loaded");
+                Feature image = new Feature("100_3.bmp", false);
+                System.Console.WriteLine("picture loaded");
 
-            double[] rez = mlp.compute(image.Pict);
-            System.Console.WriteLine(rez.Length);
-            for (int i = 0; i < rez.Length; i++)
-                System.Console.Write(rez[i].ToString() + " ");
-
+                double[] rez = mlp.compute(image.Pict);
+                System.Console.WriteLine(rez.Length);
+                for (int i = 0; i < rez.Length; i++)
+                    System.Console.Write(rez[i].ToString() + " ");
+            }
+            
+            
             System.Console.WriteLine("done...");
             System.Console.ReadKey();
         }
