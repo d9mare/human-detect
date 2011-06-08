bcd70ebadfb8c6a967c31116656202993d9304b6
janca.andrei@gmail.com <janca.andrei@gmail.com@489428b5-2de9-d710-ef6a-e1c4df9b4134>
2011-06-08 15:13:20 +0000

    codul de XOR e comentat, sa nu uiti sa schimbi caile catre directorul cu imagini

--- MLPClassifier/MLPClassifier/MainClass.cs
+++ MLPClassifier/MLPClassifier/MainClass.cs
@@ -15,29 +15,30 @@ namespace MLPClassifier
             bool learn = true;
             String[] posStr = {"falling", "lying", "sitting", "standing"};
             MLP[] mlp = new MLP[NR_POS];
-           // if (learn == true)
-           // {
+            if (learn == true)
+            {
                 for (int i = 3; i < NR_POS; i++)
                 {
-                    mlp[i] = new MLP(30, 30, new int[] { 50, 5, 1});
+
+                    mlp[i] = new MLP(30, 30, new int[] {150, 10, 1});
                     System.Console.WriteLine("init network " + i);
-                    mlp[i].learn(@"e:\Poli\Master\Proiect cercetare\code\trunk\images\SMALLTEST", i);
+                    mlp[i].learn(@"e:\eclipse workspace\FeatGen\images\SMALLTEST", i);
                     mlp[i].save("neuralnet_"+i+".txt");
                 }
                 System.Console.WriteLine("done...");
-                //System.Console.Read();
+                System.Console.Read();
                 
-         //   }
-         //   else
-         //   {
-            /*
+            }
+             else
+            {
+            
                 for (int  i = 3; i < NR_POS; i++)
                 {
                     mlp[i] = new MLP("neuralnet_"+i+".txt");
                     System.Console.WriteLine("NN loaded "+i);
 
                 }
-            */
+         
                 string[] filePaths = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bmp", SearchOption.AllDirectories);
                 for (int k = 0; k < filePaths.Length; k++)
                 {
@@ -51,12 +52,34 @@ namespace MLPClassifier
                             System.Console.WriteLine(rez[j].ToString() + " ");
                     }
                 }
-                System.Console.WriteLine("done...");
-                System.Console.Read();
                 
-        //    }
-            
-            
+                
+                /*
+                 * XOR test
+                 * 
+                 * double[] rez = mlp[3].compute(new double[]{0,0});
+                System.Console.WriteLine("0 0 ");
+                for (int j = 0; j < rez.Length; j++)
+                    System.Console.WriteLine(rez[j].ToString() + " ");
+
+                 rez = mlp[3].compute(new double[] { 0, 1 });
+                System.Console.WriteLine("0 1 " );
+                for (int j = 0; j < rez.Length; j++)
+                    System.Console.WriteLine(rez[j].ToString() + " ");
+
+                 rez = mlp[3].compute(new double[] { 1, 0 });
+                System.Console.WriteLine("1 0 " );
+                for (int j = 0; j < rez.Length; j++)
+                    System.Console.WriteLine(rez[j].ToString() + " ");
+
+                 rez = mlp[3].compute(new double[] { 1, 1 });
+                System.Console.WriteLine("1 1 " );
+                for (int j = 0; j < rez.Length; j++)
+                    System.Console.WriteLine(rez[j].ToString() + " ");
+                System.Console.WriteLine("done...");
+                System.Console.Read();*/
+                
+             }
             
         }
     }
